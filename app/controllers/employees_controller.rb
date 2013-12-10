cb872e071cc0dc10a8218ae280484b2f235772a2
macheteshuffle <jeff.li029@gmail.com>
2013-12-10 12:10:30 -0500

    loaded Employee objects into Employee controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,7 @@
 class EmployeesController < ApplicationController
   def index
     # make your employee objects available to the correlating view here
-    @employees = Employees.all
+
+    @employees = Employee.load_employees
   end
 end
